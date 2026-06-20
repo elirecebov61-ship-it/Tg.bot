8f7f0c1537983f279a8271df17c4bc30af0d4002
elirecebov61-ship-it <elirecebov61@gmail.com>
2026-06-21 03:56:42 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ API_HASH       = os.environ["API_HASH"]
 STRING_SESSION = os.environ["STRING_SESSION"]
 
 # Yalnız bu 2 nəfər istifadə edə bilər
-ALLOWED = {8034872992, 8883308932}
+ALLOWED = {8034872992, 8793739928}
 
 pyro = Client(
     "ban_guard",
